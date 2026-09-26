2ada48ceb87bc8ce2dfd0298211487d4df96fc87
Alexey <alexivanovhq@gmail.com>
2026-09-26 15:04:33 +0300

    Include 404 page in production build

--- scripts/build-assets.cjs
+++ scripts/build-assets.cjs
@@ -2,12 +2,12 @@
 const fs=require('node:fs'),path=require('node:path');
 const root=path.resolve(__dirname,'..'),out=path.join(root,'public');
 fs.mkdirSync(out,{recursive:true});
-for(const name of ['index.html','favicon.png','robots.txt','sitemap.xml','css','js','locales','data','Account','Exploring-Space','Gallery','Latest-Space-News','More-About-Orbonix','search','Solar-System-Simulation']) {
+for(const name of ['index.html','404.html','favicon.png','robots.txt','sitemap.xml','css','js','locales','data','Account','Exploring-Space','Gallery','Latest-Space-News','More-About-Orbonix','search','Solar-System-Simulation']) {
   fs.cpSync(path.join(root,name),path.join(out,name),{recursive:true});
 }
 
 const BASE='https://orbonix.net';
-const excluded=new Set(['Account/index.html','search/index.html']);
+const excluded=new Set(['404.html','Account/index.html','search/index.html']);
 const pretty=s=>s
   .replace(/-/g,' ')
   .replace(/\bQuizes\b/gi,'Quizzes')
